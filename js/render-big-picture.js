037e0ac5e5498ade590c98777b428eb90a490501
Макс <maxkalimullin@gmail.com>
2021-03-24 07:10:48 +0300

    Перломутровые пуговицы (часть 2) - нужна помощь

--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -1,10 +1,12 @@
-//import { postsFeed } from './data.js';
+const MIN_COMMENTS_COUNT = 5;
+
+let displayCommentsCount = MIN_COMMENTS_COUNT;
 
 // находим DOM-элементы
 const body = document.body;
 const smallPicturesContainer = document.querySelector('.pictures');
 const bigPictureContainer = document.querySelector('.big-picture');
-const commentCount = bigPictureContainer.querySelector('.social__comment-count');
+const commentCounter = bigPictureContainer.querySelector('.social__comment-count');
 const commentsLoader = bigPictureContainer.querySelector('.comments-loader');
 const closeButton = bigPictureContainer.querySelector('.big-picture__cancel')
 const commentContainer = bigPictureContainer.querySelector('.social__comments');
@@ -17,10 +19,64 @@ const commentTemplate = document.querySelector('#comment')
 const commentsFragment = document.createDocumentFragment();
 
 // функция закрытия большой фотографии
-const closeBigPicture = () => {
+const onCloseButtonClick = () => {
   bigPictureContainer.classList.add('hidden');
   body.classList.remove('modal-open');
   commentContainer.textContent = '';
+  displayCommentsCount = MIN_COMMENTS_COUNT;
+  // console.log('закрыто ' + displayCommentsCount);
+}
+
+
+const clearComments = () => {
+  const renderedComments = bigPictureContainer.querySelectorAll('.social__comment');
+  renderedComments.forEach((el) => {
+    commentContainer.removeChild(el);
+  });
+}
+
+const addComments = (obj) => {
+  clearComments();
+  displayCommentsCount += MIN_COMMENTS_COUNT;
+  renderComments(obj);
+  // console.log('при клике ' + displayCommentsCount);
+}
+
+const showCommentsCounter = (obj) => {
+  if (obj.comments.length > 0) {
+    commentCounter.innerHTML = `${commentContainer.children.length} из <span class="comments-count">${obj.comments.length}</span> комментариев`;
+  }
+}
+
+const renderComments = (obj) => {
+
+  const onCommentsLoaderClick = () => addComments(obj);
+
+  const showCommentsLoader = (obj) => {
+    if (displayCommentsCount <= obj.comments.length) {
+      commentsLoader.classList.remove('hidden');
+      commentsLoader.addEventListener('click', onCommentsLoaderClick);
+    } else {
+      commentsLoader.classList.add('hidden');
+      commentsLoader.removeEventListener('click', onCommentsLoaderClick);
+    }
+  }
+
+  obj.comments
+    .slice(0, displayCommentsCount)
+    .forEach(({ avatar, message, name }) => {
+      const newComment = commentTemplate.cloneNode(true);
+      newComment.querySelector('.social__picture').src = avatar;
+      newComment.querySelector('.social__picture').alt = name;
+      newComment.querySelector('.social__text').textContent = message;
+      commentsFragment.appendChild(newComment);
+    })
+  commentContainer.appendChild(commentsFragment);
+  // console.log('при рендере ' + displayCommentsCount)
+
+
+  showCommentsCounter(obj);
+  showCommentsLoader(obj);
 }
 
 // цикл, который связывает превью фото с отрисовкой большой фотографии
@@ -39,26 +95,15 @@ const renderBigPicture = (data) => {
       bigPictureContainer.querySelector('.comments-count').textContent = data[i].comments.length;
       bigPictureContainer.querySelector('.social__caption').textContent = data[i].description;
 
-      data[i].comments.forEach(({ avatar, message, name }) => {
-        const newComment = commentTemplate.cloneNode(true);
-        newComment.querySelector('.social__picture').src = avatar;
-        newComment.querySelector('.social__picture').alt = name;
-        newComment.querySelector('.social__text').textContent = message;
-        commentsFragment.appendChild(newComment);
-      })
-      commentContainer.appendChild(commentsFragment);
+      renderComments(data[i]);
 
       body.classList.add('modal-open');
-      commentCount.classList.add('hidden');
-      commentsLoader.classList.add('hidden');
 
-      closeButton.addEventListener('click', () => {
-        closeBigPicture()
-      })
+      closeButton.addEventListener('click', onCloseButtonClick)
 
       document.addEventListener('keydown', (evt) => {
         if (evt.key === ('Escape' || 'Esc')) {
-          closeBigPicture();
+          onCloseButtonClick();
         }
       })
     })
